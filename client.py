a19403a94118c1725560aa9cb3721c8b8c17941d
Junoh Moon <mjo970625@gmail.com>
2022-03-22 00:18:21 +0900

    Let client work interactively

--- client.py
+++ client.py
@@ -1,8 +1,9 @@
-from toolz.curried import pipe
+#!/usr/bin/env python3
 import logging
 
 from google.protobuf.wrappers_pb2 import StringValue
 import grpc
+from toolz.curried import pipe
 
 from proto.storage_pb2 import Pair
 from proto.storage_pb2_grpc import StorageStub
@@ -12,19 +13,39 @@ def main():
 	with grpc.insecure_channel("localhost:50051") as channel:
 		stub = StorageStub(channel)
 
-		result = stub.put(Pair(key="hello", value="world"))
-		logging.info(result)
-
-		pipe(StringValue(value="hello"), stub.get, logging.info)
-
-		try:
-			pipe(StringValue(value="not hello"), stub.get, logging.info)
-		except grpc.RpcError as e:
-			logging.warning(e.code())
-			logging.warning(e.details())
+		while True:
+			try:
+				job, tail = parse_command()
+				if job == "get":
+					pipe(StringValue(value=tail), stub.get, logging.info)
+				else:
+					k, v = tail
+					pipe(Pair(key=k, value=v), stub.put, logging.info)
+			except grpc.RpcError as e:
+				logging.warning(e.code())
+				logging.warning(e.details())
+			except RuntimeError:
+				continue
+			except ValueError:
+				continue
+			except EOFError:
+				break
+			except KeyboardInterrupt:
+				break
 	return
 
 
+def parse_command():
+	cmd, tail = input("Input: get <key> | set <key>:<value>\n").split(maxsplit=1)
+
+	if cmd == "get":
+		return cmd, tail
+	elif cmd == "set":
+		return cmd, tail.split(':', maxsplit=1)
+	else:
+		raise RuntimeError()
+
+
 if __name__ == "__main__":
-	logging.basicConfig(level=logging.INFO)
+	logging.basicConfig(level=logging.INFO, format='[%(asctime)s - %(name)s - %(levelname)s] %(message)s')
 	main()
