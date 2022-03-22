f3ff8a0cd520e493b472dcf6e19f93be52af37ad
Junoh Moon <jo.moon@dco.com>
2022-03-22 17:37:17 +0900

    Discriminate dev packages

--- client.py
+++ client.py
@@ -3,7 +3,6 @@ import logging
 
 from google.protobuf.wrappers_pb2 import StringValue
 import grpc
-from toolz.curried import pipe
 
 from proto.storage_pb2 import Pair
 from proto.storage_pb2_grpc import StorageStub
